b0fd4f59852b336ed8e675e026f2a53314bb6591
amine <aaliane781@gmail.com>
2025-05-28 11:32:25 +0100

    update(nvim.lsp)

--- nvim/.config/nvim/lua/plugins/lsp/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason-lspconfig.lua
@@ -1,37 +1,39 @@
 return {
 
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-        local mason_lspconfig = require("mason-lspconfig")
-        local mason_tool_installer = require("mason-tool-installer")
-        mason_lspconfig.setup({
-            ensure_installed = {
-                "html",
-                "cssls",
-                "tailwindcss",
-                "lua_ls",
-                "graphql",
-                "emmet_ls",
-                "prismals",
-                "pyright",
-                "gopls",
-                "marksman",
-                "ts_ls",
-            },
-        })
-        mason_tool_installer.setup({
-            ensure_installed = {
-                "prettier",
-                "stylua",
-                "isort",
-                "black",
-                "pylint",
-                "eslint_d",
-                "goimports",
-            },
-        })
-    end,
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"html",
+				"cssls",
+				"tailwindcss",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+				"gopls",
+				"marksman",
+				"ts_ls",
+				"nginx_language_server",
+			},
+		})
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"isort",
+				"black",
+				"pylint",
+				"eslint_d",
+				"goimports",
+				"nginx-language-formatter",
+			},
+		})
+	end,
 }
