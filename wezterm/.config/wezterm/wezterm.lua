358120b0b6824393125076f7bb7fe42a876fc693
aa-Aliane <aaliane781@gmail.com>
2025-06-02 08:18:44 +0100

    update(zshrc)

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 return {
-	font = wezterm.font("FiraCode Nerd Font"),
-	font_size = 12.0,
-	harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
-	color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
+    font = wezterm.font("FiraCode Nerd Font"),
+    font_size = 12.0,
+    harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
+    color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
 }
